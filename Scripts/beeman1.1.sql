f3bd7ebebd50c10f95123c7ed62e2ae0c2788afc
DanielBeeman <33970215+DanielBeeman@users.noreply.github.com>
2024-09-25 01:26:11 +0000

    Challenge 1.1

--- Scripts/beeman1.1.sql
+++ Scripts/beeman1.1.sql
@@ -1,4 +1,4 @@
 select e1.firstName, e1.lastName, e2.firstName as 'mgr first name', e2.lastName as 'mgr last name'
 from  employee e1
 JOIN  employee e2
-on   e1.employeeId = e2.managerId
+on   e1.managerId = e2.employeeId
